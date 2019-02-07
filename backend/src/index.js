80e598d19592a5aed0268c0db112b125a91a3aa2
Andrews Henrique Drein <ahdrein@users.noreply.github.com>
2019-02-07 08:04:20 -0200

    Using mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,12 @@
 const express = require("express")
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://ahdrein:ahdrein123@ds123645.mlab.com:23645/goweek-ahdrein-backend",
+{
+    useNewUrlParser: true
+})
 
 app.get("/", (req, res) => {
     return res.send("Hello World")
